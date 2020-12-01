1d5e0b8edddc9479ad2e9edd168bb8aa46c41364
Naman Singh <naman.icebed@gmail.com>
2020-12-01 13:49:47 +0530

    class

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,41 @@
 import React from "react";
-import axios from "axios";
 import { getPosts } from "../actions/posts";
 import { connect, useDispatch, useSelector } from "react-redux";
 
-const App = () => {
-  const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
-  return (
-    <div>
-      <button type="submit" onClick={() => dispatch(getPosts())}>
-        Get Posts
-      </button>
-      {posts.allPosts ? <div>posts fetched</div> : <div>no posts</div>}
-    </div>
-  );
+// const App = () => {
+//   const dispatch = useDispatch();
+//   const posts = useSelector((state) => state.posts);
+//   return (
+//     <div>
+//       <button type="submit" onClick={() => dispatch(getPosts())}>
+//         Get Posts
+//       </button>
+//       {posts.allPosts ? <div>posts fetched</div> : <div>no posts</div>}
+//     </div>
+//   );
+// };
+// export default App;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type="submit" onClick={() => this.props.getPosts()}>
+          Get Posts
+        </button>
+        {this.props.posts.allPosts ? (
+          <div>posts fetched</div>
+        ) : (
+          <div>no posts</div>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log(state);
+
+  return { posts: state.posts };
 };
 
-export default App;
-
-// const mapStateToProps = (state) => {
-//   console.log(state);
-
-//   return { posts: state.posts };
-// };
-
-// export default connect(mapStateToProps, { getPosts })(App);
+export default connect(mapStateToProps, { getPosts })(App);
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,12 +2,13 @@ import { GET_POSTS } from "../actions/types";
 
 const INITIAL_STATE = {
   allPosts: null,
+  allComments: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case GET_POSTS:
-      return { allPosts: action.payload };
+      return { ...state, allPosts: action.payload };
     default:
       return state;
   }
